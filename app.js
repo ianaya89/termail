3bf56bad1e6e7e2b666ca36942f9544293164083
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-10-08 19:46:58 -0300

    Adding mailing functionality

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var inquirer = require('inquirer');
+var nodemailer = require('nodemailer');
+require('dotenv').load();
 
 var to = {
   type: 'input',
@@ -6,6 +8,55 @@ var to = {
   message: 'to ?'
 };
 
+var from = {
+  type: 'input',
+  name: 'from',
+  message: 'from ?'
+};
 
-inquirer.prompt([to], function( answers ) {
+var subject = {
+  type: 'input',
+  name: 'subject',
+  message: 'subject ?'
+};
+
+var body = {
+  type: 'input',
+  name: 'body',
+  message: 'body ?'
+};
+
+var html = {
+  type: 'confirm',
+  name: 'html',
+  message: 'html ?'
+};
+
+inquirer.prompt([from, to, subject, body, html], function( answers ) {
+  var opt = {
+    from: answers.from,
+    to: answers.to,
+    subject: answers.subject
+  };
+
+
+  sendEmail(opt);
 });
+
+function sendEmail(opt) {
+  var transporter = nodemailer.createTransport({
+    service: process.env.PROVIDER,
+    auth: {
+      user: process.env.FROM,
+      pass: process.env.PASS
+    }
+  });
+
+  transporter.sendMail(opt, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+
+  });
+}
